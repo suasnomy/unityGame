3d53a8cdac5a2c0a192ed913ae60505cbdc24ebf
Bishal Poudel <bishalpoudel563@gmail.com>
2023-03-11 18:16:58 +0545

    Completed #7 , downloaded the music assest

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -34,26 +37,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
-        //if (Input.GetKey("up"))
-        //{
-        //    rb.velocity = new Vector3(0, 0, 5f);
-        //}
-
-        //if (Input.GetKey("right"))
-        //{
-        //    rb.velocity = new Vector3(5f, 0, 0);
-        //}
-
-        //if (Input.GetKey("left"))
-        //{
-        //    rb.velocity = new Vector3(-5f, 0, 0);
-        //}
-
-        //if (Input.GetKey("down"))
-        //{
-        //    rb.velocity = new Vector3(0, 0, -5f);
-        //}
-
+      
        
 
     }
